c53dfc8a0bb4de883de2665ac43315b6a2fbd39d
perochorichard <iamthedevelopernow@gmail.com>
2019-11-23 14:30:23 -0500

    corrected views to Cocoa touch

---
+++ GotPark/GotPark/view/TestVC.swift
@@ -0,0 +1,21 @@
+//
+//  TestVC.swift
+//  GotPark
+//
+//  Created by Richard Perocho on 2019-11-23.
+//  Copyright © 2019 Connor Clarkson. All rights reserved.
+//
+
+import SwiftUI
+
+struct TestVC: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TestVC_Previews: PreviewProvider {
+    static var previews: some View {
+        TestVC()
+    }
+}
