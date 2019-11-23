063a91096b06e7d8604f30fc7b6a599186cef6b0
Connor <clarksoc@sheridancollege.ca>
2019-11-23 17:24:04 -0500

    AppManual 80% Complete, Added ImageView

--- GotPark/GotPark/view/AppManualVC.swift
+++ GotPark/GotPark/view/AppManualVC.swift
@@ -7,24 +7,25 @@
 //
 
 import UIKit
+import WebKit
 
-class AppManualVC: UIViewController {
+class AppManualVC: UIViewController, WKUIDelegate  {
+    @IBOutlet var webView: WKWebView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        let url = Bundle.main.url(forResource: "AppManual", withExtension: "html")
+        let requestObj = URLRequest(url: url!);
+        webView.load(requestObj);
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func loadView() {
+        let webConfiguration = WKWebViewConfiguration()
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        webView.uiDelegate = self
+        view = webView
     }
-    */
+    
 
 }
