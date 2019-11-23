c0c8419d14237c916783703dd493c7fec5e624ce
Connor <clarksoc@sheridancollege.ca>
2019-11-23 14:13:00 -0500

    Added Customer Support

---
+++ GotPark/GotPark/view/CustomerSupportVC.swift
@@ -0,0 +1,30 @@
+//
+//  CustomerSupport.swift
+//  GotPark
+//
+//  Created by Connor Clarkson on 2019-11-23.
+//  Copyright © 2019 Connor Clarkson. All rights reserved.
+//
+
+import UIKit
+
+class CustomerSupportVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
