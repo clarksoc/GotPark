f0f471da51d0111bcaa07b9b1fa4088a1b469fcc
Connor Clarkson <49419137+clarksoc@users.noreply.github.com>
2019-11-23 14:00:19 -0500

    Add files via upload
    Initial Project

---
+++ GotPark/GotPark/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GotPark
+//
+//  Created by Connor Clarkson on 2019-11-23.
+//  Copyright © 2019 Connor Clarkson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
