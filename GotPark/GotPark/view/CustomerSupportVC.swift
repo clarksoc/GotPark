e28d07b7205d55938cc4b1905803af87fd43034c
Connor <clarksoc@sheridancollege.ca>
2019-11-23 15:10:23 -0500

    Customer Support ViewController Finished -- Need Adrian to Test with real Phone

--- GotPark/GotPark/view/CustomerSupportVC.swift
+++ GotPark/GotPark/view/CustomerSupportVC.swift
@@ -16,15 +16,21 @@ class CustomerSupportVC: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func supportEmailClick(){
+        let email = "GotParkCustomerSupport@park.ca"
+        
+        let url = URL(string: "mailto:\(email)")!
+        
+        UIApplication.shared.open(url, options: [:], completionHandler: nil)
     }
-    */
+    
+
+    @IBAction func supportPhoneClick(){
+        let phone = 18004687275
+        
+        let url = URL(string: "tel://\(phone)")!
+        
+        UIApplication.shared.open(url, options: [:], completionHandler: nil)
+    }
 
 }
