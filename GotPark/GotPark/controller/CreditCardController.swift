b28586a5ab9c525a94d12eee78931cca4efed3ab
Adrian <adriangonzalezmadruga@gmail.com>
2019-12-12 16:54:54 -0500

    Added controllers and signup and signin UI

---
+++ GotPark/GotPark/controller/CreditCardController.swift
@@ -0,0 +1,9 @@
+//
+//  CreditCardController.swift
+//  GotPark
+//
+//  Created by Adrian Gonzalez Madruga on 2019-12-12.
+//  Copyright © 2019 Connor Clarkson. All rights reserved.
+//
+
+import Foundation
