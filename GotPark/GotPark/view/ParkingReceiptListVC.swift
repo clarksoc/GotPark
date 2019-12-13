cb2aeec24c9e81da301f35731afdfefcd3c88b1f
Adrian <adriangonzalezmadruga@gmail.com>
2019-12-13 14:46:30 -0500

    Working On ReceiptList

--- GotPark/GotPark/view/ParkingReceiptListVC.swift
+++ GotPark/GotPark/view/ParkingReceiptListVC.swift
@@ -9,22 +9,56 @@
 import UIKit
 
 class ParkingReceiptListVC: UITableViewController {
-
+    
+    private var receiptList = [Receipt]()
+    let receiptController = ReceiptController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.title = "ReceiptList"
+        
+        let newIndex = receiptList.count
+        
+        let allReceipts = (receiptController.getAllReceipts() ?? nil)!
+        
+        // Create new item and add it to the task list
+        if (allReceipts != nil){
+            for receipts in allReceipts{
+                let code = task.value(forKey: "buildingCode") as! Integer64
+                let duration = task.value(forKey: "duration") as! Integer64
+                let parkCost = task.value(forKey: "parkCost") as! Integer64
+                let suiteNumber = task.value(forKey: "suiteNumber") as! Integer64
+                let parkPlate = task.value(forKey: "parkPlate") as! Integer64
+                let date = task.value(forKey: "date") as! Date
+                var usedReceipt = Receipt(buildingCode: code, date: date, duration: duration, parkingCost: parkCost, parkingPlate: parkPlate, suiteNumber: suiteNumber)
+                receiptList.append(usedReceipt)
+            }
+        }
+        
+        // Tell the table view a new row has been created
+        tableView.insertRows(at: [IndexPath(row: newIndex, section: 0)], with: .top)
+        tableView.reloadData()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
-    */
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return receiptList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell_receipt", for: indexPath) as! ParkingReceiptListCell
+        
+        if indexPath.row < receiptList.count
+        {
+            let receipt = receiptList[indexPath.row]
+            cell.lblBuildingCode?.text = "\(receipt.buildingCode)"
+        }
+        
+        return cell
+    }
 
 }
---
+++ GotPark/GotPark/view/ParkingRecieptListCell.swift
@@ -0,0 +1,27 @@
+//
+//  ParkingRecieptListCell.swift
+//  GotPark
+//
+//  Created by Adrian Gonzalez Madruga on 2019-12-13.
+//  Copyright © 2019 Connor Clarkson. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ParkingReceiptListCell: UITableViewCell {
+
+    @IBOutlet weak var lblBuildingCode: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
